713235f266b345a0a6d4ecb18b090a6410323272
Danny Huggins <dannyhuggins@proton.me>
2025-08-25 09:07:24 +0100

    put server and game name in title

--- src/Bot.php
+++ src/Bot.php
@@ -138,8 +138,8 @@ final class Bot {
   private function buildServerEmbed(Discord $discord, array $serverInfo): Embed {
     $embed = new Embed($discord);
     $embed
-      ->setTitle($serverInfo['name'])
-      ->setAuthor($serverInfo['game'])
+      ->setTitle("{$serverInfo['name']} ({$serverInfo['game']})")
+      // ->setAuthor($serverInfo['game'])
       ->setColor($serverInfo['statusColor'])
       ->setDescription($serverInfo['description'])
       ->addFieldValues('Address', "```{$serverInfo['address']}```", true);
